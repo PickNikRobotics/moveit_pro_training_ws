3e895e8eefdce13fdfc07085cb0f37dfed53f5f2
Sebastian Castro <sebas.a.castro@gmail.com>
2023-06-01 21:57:16 -0400

    Complete the full ROS 2 pipeline and add test image

--- src/apriltag_ros_python/src/apriltag_ros_python/apriltag_detection_client.py
+++ src/apriltag_ros_python/src/apriltag_ros_python/apriltag_detection_client.py
@@ -11,8 +11,9 @@ from threading import Thread
 
 
 class AprilTagDetectionClient(Node):
-    def __init__(self):
+    def __init__(self, test_mode=False):
         super().__init__("apriltag_detection_client")
+        self.test_mode = test_mode
 
         self.client = self.create_client(GetAprilTagDetections, "detect_apriltags")
         while not self.client.wait_for_service(timeout_sec=2.0):
@@ -33,14 +34,17 @@ class AprilTagDetectionClient(Node):
         )
         self.get_logger().info("Waiting for camera info...")
 
-        # Get image from camera
-        self.latest_image = None
-        self.image_sub = self.create_subscription(
-            Image,
-            "/wrist_mounted_camera/color/image_raw",  # TODO: Do not hardcode
-            self.image_callback,
-            qos_profile_sensor_data,
-        )
+        # Get image from camera or from a test image
+        if not self.test_mode:
+            self.latest_image = None
+            self.image_sub = self.create_subscription(
+                Image,
+                "/wrist_mounted_camera/color/image_raw",  # TODO: Do not hardcode
+                self.image_callback,
+                qos_profile_sensor_data,
+            )
+        else:
+            self.latest_image = self.load_test_image()
 
         # Wait to have data
         while not self.camera_info:
@@ -60,7 +64,9 @@ class AprilTagDetectionClient(Node):
             self.rate.sleep()
 
         response = self.future.result()
-        print(f"Got response:\n{response}")
+        num_detections = len(response.detections)
+        detection_ids = [det.id for det in response.detections]
+        print(f"Got response with {num_detections} detections.\nIDs: {detection_ids}")
         return response
 
     def camera_info_callback(self, msg):
@@ -73,11 +79,26 @@ class AprilTagDetectionClient(Node):
         self.latest_image = msg
         self.destroy_subscription(self.image_sub)
 
+    def load_test_image(self):
+        """Loads a test image with AprilTags."""
+        import cv2
+        import cv_bridge
+        import os
+        from ament_index_python.packages import get_package_share_directory
+
+        data_dir = os.path.join(
+            get_package_share_directory("apriltag_ros_python"), "data"
+        )
+        img_file = os.path.join(data_dir, "test_image.png")
+        img = cv2.imread(img_file)
+        bridge = cv_bridge.CvBridge()
+        return bridge.cv2_to_imgmsg(img, encoding="bgr8")
+
 
 def main():
     rclpy.init()
 
-    node = AprilTagDetectionClient()
+    node = AprilTagDetectionClient(test_mode=True)
 
     Thread(target=node.send_request).start()
     rclpy.spin(node)
--- src/apriltag_ros_python/src/apriltag_ros_python/apriltag_detection_server.py
+++ src/apriltag_ros_python/src/apriltag_ros_python/apriltag_detection_server.py
@@ -7,9 +7,12 @@ from apriltag_ros_msgs.srv import GetAprilTagDetections
 import cv2
 import cv_bridge
 import dt_apriltags
+from geometry_msgs.msg import PoseStamped
+import numpy as np
 import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
+from scipy.spatial.transform import Rotation
 
 
 class AprilTagDetectionServer(Node):
@@ -34,12 +37,51 @@ class AprilTagDetectionServer(Node):
     def get_detections_callback(self, request, response):
         self.get_logger().info("Got incoming request")
 
+        # Detect tags
         img = self.bridge.imgmsg_to_cv2(request.image, desired_encoding="bgr8")
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        tags = self.detector.detect(img_gray, True)
-
+        tags = self.detector.detect(
+            img_gray,
+            estimate_tag_pose=True,
+            camera_params=self.msg_to_camera_params(request.camera_info),
+            tag_size=1.0,
+        )
         self.get_logger().info(f"Got tags:\n{tags}")
+
+        # Package them up into a response
+        frame = request.camera_info.header.frame_id
+        for tag in tags:
+            response.detections.append(self.tag_to_msg(tag, frame))
+
         return response
+
+    def msg_to_camera_params(self, msg):
+        """
+        Converts a ROS CameraInfo message to AprilTag detector camera parameters.
+        """
+        return (msg.k[0], msg.k[4], msg.k[2], msg.k[5])
+
+    def tag_to_msg(self, tag, frame):
+        """
+        Converts a single AprilTag detection to a ROS message.
+        """
+        msg = Detection()
+        msg.family = tag.tag_family.decode()
+        msg.id = tag.tag_id
+
+        pose = PoseStamped()
+        pose.header.frame_id = frame
+        position = tag.pose_t.flatten()
+        pose.pose.position.x = position[0]
+        pose.pose.position.y = position[1]
+        pose.pose.position.z = position[2]
+        q = Rotation.from_matrix(tag.pose_R.astype(np.float32)).as_quat()
+        pose.pose.orientation.x = q[0]
+        pose.pose.orientation.y = q[1]
+        pose.pose.orientation.z = q[2]
+        pose.pose.orientation.w = q[3]
+        msg.pose = pose
+        return msg
 
 
 def main(args=None):
