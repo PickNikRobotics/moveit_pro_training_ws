4514f4a8c80427081ed651f18a8b0a9fe42d6118
Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
2023-06-28 10:20:40 -0600

    Update to version 2.3 (#18)

--- src/moveit_studio_training_behaviors/src/register_behaviors.cpp
+++ src/moveit_studio_training_behaviors/src/register_behaviors.cpp
@@ -4,26 +4,26 @@
 
 #include <moveit_studio_training_behaviors/get_apriltag_detection_pose.hpp>
 #include <moveit_studio_training_behaviors/setup_mtc_pick_from_pose.hpp>
-#include <moveit_studio_training_behaviors/transform_pose.hpp>
+#include <moveit_studio_training_behaviors/offset_pose.hpp>
 
 #include <pluginlib/class_list_macros.hpp>
 
 namespace moveit_studio_training_behaviors
 {
-class MoveItStudioTrainingBehaviorsLoader : public moveit_studio::behaviors::SharedResourcesNodeLoaderBase
-{
-public:
-  void registerBehaviors(BT::BehaviorTreeFactory &factory,
-                         const std::shared_ptr<moveit_studio::behaviors::BehaviorContext> &shared_resources) override
+  class MoveItStudioTrainingBehaviorsLoader : public moveit_studio::behaviors::SharedResourcesNodeLoaderBase
   {
-    using namespace moveit_studio::behaviors;
+  public:
+    void registerBehaviors(BT::BehaviorTreeFactory &factory,
+                           const std::shared_ptr<moveit_studio::behaviors::BehaviorContext> &shared_resources) override
+    {
+      using namespace moveit_studio::behaviors;
 
-    registerBehavior<GetAprilTagDetectionPose>(factory, "GetAprilTagDetectionPose", shared_resources);
-    registerBehavior<SetupMTCPickFromPose>(factory, "SetupMTCPickFromPose", shared_resources);
-    registerBehavior<TransformPose>(factory, "TransformPose");
-  }
-};
-}  // namespace moveit_studio_training_behaviors
+      registerBehavior<GetAprilTagDetectionPose>(factory, "GetAprilTagDetectionPose", shared_resources);
+      registerBehavior<SetupMTCPickFromPose>(factory, "SetupMTCPickFromPose", shared_resources);
+      registerBehavior<OffsetPose>(factory, "OffsetPose");
+    }
+  };
+} // namespace moveit_studio_training_behaviors
 
 PLUGINLIB_EXPORT_CLASS(moveit_studio_training_behaviors::MoveItStudioTrainingBehaviorsLoader,
                        moveit_studio::behaviors::SharedResourcesNodeLoaderBase);
